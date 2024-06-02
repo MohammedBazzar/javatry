82b04514b13dea1bcc559ac2e86e93610587199c
MohammedBazzar <momobazzar12@gmail.com>
2024-06-02 21:40:48 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector(".userProfile h2").textContent="HEllo Mohammed";
+console.log(document.querySelector(".userProfile"));
+document.querySelector(".userInfo p").classList.add("hell");
+console.log(document.querySelector(".userInfo p").classList.add("bgg"));
